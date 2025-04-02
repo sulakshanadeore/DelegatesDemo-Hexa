9cdcc5b127ff888a136d66c10f3c73d91d2ab98d
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-02 10:52:03 +0530

    Add files via upload

---
+++ Enrollments.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForDelegates
+{
+    public delegate void CourseFees(int f1, int f2);//Step 2 Declare a declare which references the method
+
+    public class Enrollments//Step 1 create the class with the Method
+    {
+        static int _receiptno=1;
+        public int TotalFees { get; set; }
+        public double Discount { get; set; }
+        public double FeesAfterDiscount { get; set; }
+
+        public string PaymentMode { get; set; }
+        public int ReceiptNo { get; set; }
+        public void CalculateTotalCourseFees(int crsfees1, int crsfeees2)
+        {
+            TotalFees = crsfees1 + crsfeees2;
+
+        }
+
+        public void CalculateDiscountOnFees(int crsfees1, int crsfeees2)
+        {
+
+            if ((crsfees1 > 10000) || (crsfeees2 > 10000))
+            {
+                Discount=TotalFees * .10;
+                FeesAfterDiscount = TotalFees - Discount;
+
+            }
+            else 
+            {
+                Discount = 0;
+            }
+        
+        }
+
+
+        public void GenerateReceipt(int crsfees1, int crsfeees2)
+        {
+            _receiptno++;
+            ReceiptNo=_receiptno;
+            PaymentMode = "Cash";
+
+        
+        }
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿namespace LibraryForDelegates
+{
+    public delegate void TotalMarksCalculation();//Step 2 Declare a declare which references the method
+
+    public delegate float PercentageCalulation();
+    public delegate bool StudentResult(float p);
+    public class Student //Step 1 create the class with the Method
+    {
+        public int RollNo { get; set; }
+        public string Name {get; set; }
+        public int TotalMarks { get; set; }
+        public int Sub1 { get; set; }
+        public int Sub2 { get; set; }
+        public int Sub3 { get; set; }
+        public void CalculateTotalMarks()
+        {
+            TotalMarks = Sub1 + Sub2 + Sub3;
+        }
+
+        public float CalculatePercentage()
+        {
+            float percentage = (TotalMarks*100) / 300;
+            return percentage;
+       }
+
+        public bool OverAllResult(float percentage)
+        {
+            bool result = false;
+            if (percentage >= 75)
+            {
+                result = true;
+
+            }
+            return result;
+        
+        }
+
+    }
+}
